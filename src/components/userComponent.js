83def043cbeb87630a6aaed279ff6d8e1ba97353
baht <baht@bil.omu.edu.tr>
2021-11-23 14:34:50 +0300

    22.11.2021-Ders Sonu

--- src/components/userComponent.js
+++ src/components/userComponent.js
@@ -1,4 +1,5 @@
 import BaseLogger, { ElasticLogger, MongoLogger } from "../crossCuttingConcerns/logger.js"
+import Customer from "../models/customer.js"
 import User from "../models/user.js"
 import UserService from "../services/userService.js"
 
@@ -13,10 +14,10 @@ let user2 = new User(2, "Meliha", "Bahtiyar", "Merzifon")
 userService.add(user1)
 userService.add(user2)
 
-console.log(userService.list())
+//console.log(userService.list())
 
-console.log(userService.getById(2))
-userService.list()
+//console.log(userService.getById(2))
+//userService.list()
 
 
 //prototyping----------------------------------------------------------
@@ -24,3 +25,17 @@ let customer = { id: 1, firstName: "Mert" }
 customer.lastName = "Bahtiyar"
 
 console.log(customer.lastName)
+
+console.log("------------------------------------")
+
+userService.load()
+
+
+let customerToAdd = new Customer(1, "Aziz", "Saka", "Merzifon", "fdgdfg")
+customerToAdd.type = "customer"
+
+userService.add(customerToAdd)
+console.log(userService.customers)
+console.log(userService.employees)
+console.log(userService.errors)
+console.log(userService.getCustomerSorted())
